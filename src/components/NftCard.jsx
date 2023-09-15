f6c08c715732b692e1f90cf3e6f0901d15e24d55
Anel6969 <115752275+Anel6969@users.noreply.github.com>
2023-09-14 23:45:50 -0400

    2

---
+++ src/components/NftCard.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import ethIcon from '../images/icon-ethereum.svg'
+import clockIcon from '../images/icon-clock.svg'
+import Avatar from '../images/image-avatar.jpg'
+import equilibrium from '../images/image-equilibrium.jpg'
+
+
+export class Nftcard extends Component {
+  render() {
+    return (
+      <main class="card-container">
+           <section className="nft-card">
+                <div className='card-content'>
+                    <div className='image'>
+                        <img className='eq-img' src={equilibrium}alt='equilibrium image'/>
+                    </div>
+                    <h1 className='eth-number'>Equilibrium #2597</h1>
+                    <p>Our Equilibrium collection promotes balance and calm.</p>
+                    <div className='eth-and-days'>
+                        <div className='eth'>
+                            <img className='eth-icon' src={ethIcon} alt="etc-icon"/>
+                            <p>0.041 ETH</p>
+                        </div>
+                        <div className='days-left'>
+                            <img className='clock-icon' src={clockIcon} alt="etc-icon"/>
+                            <p>3 days left</p>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className='creator'>
+                        <img className='avatar' alt='avatarpicture' src={Avatar}/>
+                        <p>Creation of <span className='name'>Anel Grebovic</span></p>
+                    </div>
+                </div>
+           </section>
+      </main>
+    )
+  }
+}
+
+export default Nftcard
